7409a3793e007018fa9987c24b616bce7ee5a66e
Alexey Popov <lepa1984@mail.ru>
2022-12-30 08:36:01 +0300

    fix: sprint 4 rework 30.12.2022

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,19 @@ const openPopupButton = document.querySelector(".profile__button-edit");
 const popup = document.querySelector(".popup");
 const popupClose = popup.querySelector(".popup__close");
 const formElement = document.querySelector(".form");
-const nameInput = formElement.querySelector(".form__input_name");
-const jobInput = formElement.querySelector(".form__input_about");
+const nameInput = formElement.querySelector(".form__input_name_value");
+const jobInput = formElement.querySelector(".form__input_about_value");
 const title = document.querySelector(".profile__title");
 const subtitle = document.querySelector(".profile__subtitle");
-
+console.log(nameInput);
 function closePopup() {
     popup.classList.remove("popup_opened");
 }
 
 function openPopup() {
     popup.classList.add("popup_opened");
+    nameInput.value = title.textContent;
+    jobInput.value = subtitle.textContent;
 }
 
 function handleFormSubmit(e) {

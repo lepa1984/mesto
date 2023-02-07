2203ba1c0d240895fe822bcf11633a7d99718b40
Alexey Popov <lepa1984@mail.ru>
2023-02-07 17:12:55 +0300

    fix: rework 07.02.2023

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,10 @@
-// Открытие попапа
-
 const openPopupProfile = document.querySelector(".profile__button-edit");
 const addCardButton = document.querySelector(".profile__button-add");
 const cardsContainer = document.querySelector(".cards");
-const icons = document.querySelectorAll(".card__heart");
 const popupAdd = document.querySelector(".popup-add");
 const popupEdit = document.querySelector(".popup-edit");
 const popupImg = document.querySelector(".popup-img");
 const closeList = document.querySelectorAll(".popup__close");
-const popupImage = popupImg.querySelector(".popup__wrapper");
 const formEdit = document.querySelector("#form-edit");
 const formAdd = document.querySelector("#form-add");
 const nameInput = formEdit.querySelector(".form__input_name_value");
@@ -47,33 +43,22 @@ const initialCards = [
     },
 ];
 
-function closePopup() {
-    if (document.querySelector(".popup.popup_opened")) {
-        document
-            .querySelector(".popup.popup_opened")
-            .classList.remove("popup_opened");
-    }
-    if (document.querySelector(".card__image_thumb")) {
-        document.querySelector(".card__image_thumb").remove();
-        document.querySelector(".card__title_thumb").remove();
-    }
+function closePopup(popup) {
+    popup.classList.remove("popup_opened");
 }
 function createCards(link, name) {
-    const cardTemplate = document.querySelector("#card").content;
     const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-
     cardElement.querySelector(".card__image").src = `${link}`;
     cardElement.querySelector(".card__image").alt = `${name}`;
     cardElement.querySelector(".card__title").textContent = `${name}`;
-    cardElement.querySelector(".card__title").title = `${name}`;
-    cardsContainer.prepend(cardElement);
+    return cardElement;
 }
 initialCards.forEach((item) => {
-    createCards(item.link, item.name);
+    renderCard(item.link, item.name);
 });
 
-function openPopup() {
-    popupEdit.classList.add("popup_opened");
+function openPopup(popup) {
+    popup.classList.add("popup_opened");
 }
 
 function clearValue(input1, input2) {
@@ -85,21 +70,22 @@ function handleFormSubmitEdit(e) {
     e.preventDefault();
     title.textContent = nameInput.value;
     subtitle.textContent = jobInput.value;
-    closePopup();
+    closePopup(popupEdit);
     clearValue(nameInput, jobInput);
 }
 function openProfilePopup() {
     nameInput.value = title.textContent;
     jobInput.value = subtitle.textContent;
-    openPopup();
+    openPopup(popupEdit);
 }
-function addPopupCard() {
-    popupAdd.classList.add("popup_opened");
+function renderCard(link, name) {
+    cardsContainer.prepend(createCards(link, name));
+    cardsContainer.addEventListener("click", clickCards);
 }
 function addCard(e) {
     e.preventDefault();
-    createCards(linkInput.value, locationInput.value);
-    closePopup();
+    renderCard(linkInput.value, locationInput.value);
+    closePopup(popupAdd);
     clearValue(locationInput, linkInput);
 }
 
@@ -111,23 +97,19 @@ function clickCards(e) {
         e.target.closest(".card").remove();
     }
     if (e.target.classList.contains("card__image")) {
-        popupImg.classList.add("popup_opened");
+        openPopup(popupImg);
         imgThumb.setAttribute("src", e.target.getAttribute("src"));
         imgThumb.setAttribute("alt", e.target.getAttribute("alt"));
-        titleThumb.setAttribute("title", e.target.getAttribute("alt"));
         titleThumb.textContent = e.target.getAttribute("alt");
-        popupImage.append(imgThumb, titleThumb);
     }
 }
 
 openPopupProfile.addEventListener("click", openProfilePopup);
 
-cardsContainer.addEventListener("click", clickCards);
-
-addCardButton.addEventListener("click", addPopupCard);
+addCardButton.addEventListener("click", () => openPopup(popupAdd));
 
 closeList.forEach((close) => {
-    close.addEventListener("click", closePopup);
+    close.addEventListener("click", () => closePopup(close.closest(".popup")));
 });
 
 formEdit.addEventListener("submit", handleFormSubmitEdit);
